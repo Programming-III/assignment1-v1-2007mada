cc46f2397affbd51dba25a3b20d878879dfd04db
Mohamed Fady <155117074+2007mada@users.noreply.github.com>
2025-11-12 15:43:09 +0200

    Define Mammal class in Mammal.h
    Added Mammal class definition with constructors and methods.

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,23 @@
-#ifndef MAMMAL_H
-#define MAMMAL_H
+//
+// Created by maham on 11/12/2025.
+//
 
+#ifndef TEST_MAMMAL_H
+#define TEST_MAMMAL_H
 #include "Animal.h"
 
-//define mammal class here
+
+class Mammal : public Animal{
+private:
+    string furColor;
+public:
+    Mammal();
+    Mammal(string furColor,string name, int age,bool isHungry);
+    ~Mammal();
+    void setFurColor(string furColor);
+    string getFurColor();
+
+};
 
 
-
-
-
-
-#endif
+#endif //TEST_MAMMAL_H
