5d09a2015369a58990dd923fda2e8eb2b5051c2d
Mohamed Fady <155117074+2007mada@users.noreply.github.com>
2025-11-12 15:44:48 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,24 @@
-#ifndef BIRD_H
-#define BIRD_H
+//
+// Created by maham on 11/12/2025.
+//
 
+#ifndef TEST_BIRD_H
+#define TEST_BIRD_H
 #include "Animal.h"
 
-//define class bird here 
+
+class Bird:public Animal {
+private:
+    float wingSpan;
+public:
+    Bird();
+
+    Bird(float wingSpan, string name, int age, bool isHungry);
+    ~Bird();
+    float getWingSpan();
+    void setWingSpan(float wingSpan);
+
+};
 
 
-
-
-
-
-
-#endif
+#endif //TEST_BIRD_H
