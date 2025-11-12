5a23d329b80517b402d55e285c87d03e29277c62
Mohamed Fady <155117074+2007mada@users.noreply.github.com>
2025-11-12 15:42:22 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,24 @@
-#ifndef VISITOR_H
-#define VISITOR_H
+//
+// Created by maham on 11/12/2025.
+//
 
+#ifndef TEST_VISITOR_H
+#define TEST_VISITOR_H
 #include <string>
+#include<iostream>
 using namespace std;
 
-//define visitor class here
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
+public:
+
+    Visitor(string visitor_name, int tickets_bought);
+    Visitor();
+    void diplayInfo();
+
+};
 
 
-
-
-
-
-
-#endif
+#endif //TEST_VISITOR_H
