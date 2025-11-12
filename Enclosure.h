5c63b8afa5940e10dcf9a3ac6aecb571a24f82fc
Mohamed Fady <155117074+2007mada@users.noreply.github.com>
2025-11-12 15:44:22 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,27 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
+//
+// Created by maham on 11/12/2025.
+//
 
+#ifndef TEST_ENCLOSURE_H
+#define TEST_ENCLOSURE_H
 #include "Animal.h"
 
-//define enclosure class here 
+
+class Enclosure {
+private:
+    Animal *animal;
+    int capacity;
+    int currentCount;
+public:
+    Enclosure();
+    Enclosure(int capacity);
+    ~Enclosure() {
+        delete[] animal;
+        animal=nullptr;
+    }
+    void addAnimal(Animal *a);
+    void displayAnimals();
+};
 
 
-
-
-
-
-
-
-#endif
+#endif //TEST_ENCLOSURE_H
